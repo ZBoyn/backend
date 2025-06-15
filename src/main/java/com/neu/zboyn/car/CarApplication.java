ca6e0cceb6b7dc0c93dedf4ed1a25a77873b6439
ZBoyn <279782706@qq.com>
2025-06-15 15:46:18 +0800

    car_backend_init

---
+++ src/main/java/com/neu/zboyn/car/CarApplication.java
@@ -0,0 +1,13 @@
+package com.neu.zboyn.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/neu/zboyn/car/mapper/UserMapper.java
@@ -0,0 +1,5 @@
+package com.neu.zboyn.car.mapper;
+
+public interface UserMapper {
+
+}
