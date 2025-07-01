ea961f4a9e319f68a2bc666df1ea62f4ff7f1bc1
ZBoyn <279782706@qq.com>
2025-07-01 16:48:47 +0800

    perm manage

--- src/main/java/com/neu/zboyn/car/mapper/UserMapper.java
+++ src/main/java/com/neu/zboyn/car/mapper/UserMapper.java
@@ -5,7 +5,6 @@ import com.neu.zboyn.car.model.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
 
 @Mapper
 public interface UserMapper {
